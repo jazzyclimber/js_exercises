948059ea504b0fd3ed92e300af38b28cf18d3ef1
jazzyclimber <jazzyclimber@gmail.com>
2019-02-19 22:06:25 -0600

    Completed Js Funciton and tested. Passed.

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+  return input.split('').reverse().join('');
 }
+
+
+
 
 module.exports = reverseString
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+  if (low < 0 || high < 0) return 'ERROR';
+  if (typeof low !== "number" || typeof high !== "number") return "ERROR";
+  if (low > high) {
+    const temp = low;
+    low = high;
+    high = temp;
+  }
 
+  //set variable for summation
+  let total = 0;
+
+  //Iterate through numbers array and add to total
+  for (let count = low; count <= high; count++) {
+    total += count;
+  }
+  return total
 }
-
 module.exports = sumAll
